b4cdb2e9c33734801a10d14f5f524d00566fc215
Sean Porter <portertech@gmail.com>
2018-04-12 15:46:23 -0700

    only enable posix spawn on x86_64 and i386
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- lib/sensu/spawn.rb
+++ lib/sensu/spawn.rb
@@ -20,6 +20,7 @@ rescue LoadError; end
 module Sensu
   module Spawn
     POSIX_SPAWN_PLATFORMS = [:linux, :macosx].freeze
+    POSIX_SPAWN_ARCHS = ["x86_64", "i386"].freeze
 
     @@mutex = Mutex.new
 
@@ -62,7 +63,9 @@ module Sensu
       # @return [TrueClass, FalseClass]
       def posix_spawn?
         return @posix_spawn unless @posix_spawn.nil?
-        @posix_spawn = POSIX_SPAWN_PLATFORMS.include?(ChildProcess.os)
+        platform_supported = POSIX_SPAWN_PLATFORMS.include?(ChildProcess.os)
+        arch_supported = POSIX_SPAWN_ARCHS.include?(ChildProcess.arch)
+        @posix_spawn = platform_supported && arch_supported
       end
 
       # Determine if the current platform is Windows.
